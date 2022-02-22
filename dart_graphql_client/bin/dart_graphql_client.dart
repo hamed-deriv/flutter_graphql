44ea91cd75f03efc3054af97fc7a3172c8196e82
hamed-fs <hamed@firstsource.io>
2022-02-22 07:58:28 +0330

    add dependencies

---
+++ dart_graphql_client/bin/dart_graphql_client.dart
@@ -0,0 +1,5 @@
+import 'package:dart_graphql_client/dart_graphql_client.dart' as dart_graphql_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_graphql_client.calculate()}!');
+}
